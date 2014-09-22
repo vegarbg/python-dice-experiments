7e1d0ed5e115519adddf186829565c468d739e53
Vegar Guldal <vegar@guldal.no>
2014-09-22 13:56:59 +0200

    Create function for dice pmf

--- experiment.py
+++ experiment.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8
 
-# Source: http://www.johndcook.com/blog/2013/04/29/rolling-dice-for-normal-samples-python-version/
 from numpy.polynomial.polynomial import polypow
 from numpy import ones
- 
-sides = 6
-dice = 3
- 
-# Create an array of polynomial coefficients for
-# x + x^2 + ... + x^sides
-p = ones(sides + 1)
-p[0] = 0
-p /= sides
- 
-# Extract the coefficients of p(x)**dice and divide by sides**dice
-pmf = polypow(p, dice)
-cdf = pmf.cumsum()
 
-import matplotlib.pyplot as plt
+def diePmf( dice, sides ):
+    # Source: http://www.johndcook.com/blog/2013/04/29/rolling-dice-for-normal-samples-python-version/
+     
+    # Create an array of polynomial coefficients for
+    # x + x^2 + ... + x^sides
+    p = ones(sides + 1)
+    p[0] = 0
+    p /= sides
+     
+    # Extract the coefficients of p(x)**dice and divide by sides**dice
+    pmf = polypow(p, dice)
+    #cdf = pmf.cumsum()
+    
+    return pmf
 
-plt.plot(pmf)
-plt.show()
+#    import matplotlib.pyplot as plt
+#
+#    plt.plot(pmf)
+#    plt.show()
--- tests/testProbability.py
+++ tests/testProbability.py
@@ -1,4 +1,18 @@
 import unittest
 
+import numpy as np
+
+import experiment
+
 class TestProbability(unittest.TestCase):
-    pass
+    def testDiePmf(self):
+        expectedOutput = np.array([
+            0.,          0.        ,  0.        ,  0.00462963,  0.01388889,
+            0.02777778,  0.0462963 ,  0.06944444,  0.09722222,  0.11574074,
+            0.125     ,  0.125     ,  0.11574074,  0.09722222,  0.06944444,
+            0.0462963 ,  0.02777778,  0.01388889,  0.00462963
+            ])
+
+        output = experiment.diePmf( 3, 6 )
+
+        np.testing.assert_allclose( expectedOutput, output )
